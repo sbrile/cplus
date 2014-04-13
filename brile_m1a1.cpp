1b11d85d5d9288b651164c3849a58542e10b1d67
Stephen Brile <sbrile@scrippsnetworks.com>
2014-04-13 15:24:41 -0400

    add files

---
+++ brile_m1a1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+	cout << "Welcome to C++ Programming!\n";
+	cout << "This is the first assignment\n";
+	cout << "by Stephen Brile";
+}
---
+++ brile_m1a2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main ()
+{
+	string Line1 = "Welcome to C++ Programming!";
+	string Line2 = "This is the first assignment";
+	string Line3 = "by Stephen Brile";
+	
+	cout << Line1 << "\n";
+	cout << Line2 << "\n";
+	cout << Line3;
+}
+
